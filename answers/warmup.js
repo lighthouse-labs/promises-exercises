b83a3dffde32a161016861bec9b844e7f90c8a2e
JoelCodes <amnshinness@yahoo.com>
2018-07-14 09:23:16 -0700

    Added tests for resolve and reject

---
+++ answers/warmup.js
@@ -0,0 +1,35 @@
+
+/*
+
+Using Promise.resolve 
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
+
+*/
+function makePromiseResolveWith3(){
+  /* Return a promise that resolves with the number 3 */
+}
+
+/*
+
+Use Promise.reject
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/reject
+
+*/
+function makePromiseRejectWithBoo(){
+  /* Return a promise that rejects with the string "Boo!" */
+}
+
+/*
+
+Use Promise Constructor
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+*/
+
+module.exports = {
+  makePromiseResolveWith3,
+  makePromiseRejectWithBoo
+};
--- test/warmup.test.js
+++ test/warmup.test.js
@@ -1,13 +1,30 @@
 /* eslint-env mocha */
+const assert = require('assert');
+const {
+  makePromiseResolveWith3,
+  makePromiseRejectWithBoo
+} = require('../answers/warmup');
 
 describe('Promise.resolve(val)', () => {
-  it('create a resolving promise');
+  it('creates a resolving promise', () => {
+    return makePromiseResolveWith3()
+      .then((val) => {
+        assert.equal(val, 3);
+      });
+  });
 });
 describe('Promise.reject(err)', () => {
-  it('creates a rejecting promise');
+  it('creates a rejecting promise', () => {
+    return makePromiseRejectWithBoo()
+      .then(() => {
+        assert.fail('This promise should have rejected, not resolved');
+      }, (err) => {
+        assert.equal(err, 'Boo!');
+      });
+  });
 });
 describe('Promise constructor "new Promise((resolve, reject) => void)', () => {
   it('creates a promise that can resolve or reject');
-  it('can be used to turn a callback into a promise');
+  it('can be used to turn a callback method into a promise');
   it('can be combined with setTimeout to create a delayed promise');
 });
