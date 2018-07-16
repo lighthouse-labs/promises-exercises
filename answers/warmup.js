097ca9c8998ba33784a4b4470945f6de55eb62cf
JoelCodes <amnshinness@yahoo.com>
2018-07-15 21:26:31 -0700

    Added more promise constructor tests and reformatted tests

--- answers/warmup.js
+++ answers/warmup.js
@@ -1,33 +1,36 @@
 
-/*
-
-Using Promise.resolve 
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
-
-*/
+/**
+ * 1) Use Promise.resolve(val) to create a promise that will resolve with the value 3
+ * 
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
+ * 
+ * @returns {Promise<3>}
+ */
 function makePromiseResolveWith3(){
-  /* Return a promise that resolves with the number 3 */
+  /* IMPLEMENT ME! */
 }
 
-/*
-
-Use Promise.reject
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/reject
-
-*/
+/**
+ * 2) Use Promise.reject to create a promise that will reject with the string "Boo!"
+ * 
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/reject
+ * 
+ * @returns {Promise<,"Boo!">}
+ */
 function makePromiseRejectWithBoo(){
-  /* Return a promise that rejects with the string "Boo!" */
+  /* IMPLEMENT ME! */
 }
 
-/*
-
-Use Promise Constructor
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-
-*/
+/**
+ * 
+ * 3) Use a Promise constructor to create a promise that will:
+ *   - resolve if itShouldResolve is truthy
+ *   - reject if itShouldResolve is falsy
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+ * 
+ * @param {boolean} itShouldResolve - Whether the promise should resolve or reject
+ * @returns {Promise<undefined, undefined>}
+ */
 
 function makePromiseWithConstructor(itShouldResolve){
   return new Promise((resolve, reject) => {
@@ -36,8 +39,62 @@ function makePromiseWithConstructor(itShouldResolve){
   });
 }
 
+/**
+ * 4) This is a common use of the Promise constructor. If you want to simulate waiting for a value, a common technique is to create a function like the following.  It simply accepts a value, and a delayInMs, then returns a promise that will resolve with that value after that delay.
+ * 
+ * @param {any} value 
+ * @param {number} delayInMs 
+ * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
+ */
+function makeDelayPromise(value, delayInMs){
+  /* Return a promise that resolves with the value after delayInMs */
+}
+
+/**
+ * 5) Here is another common use of the Promise constructor: to convert a callback-centric async function.  You can imagine a synchronous function that takes three params a, b, c, and returns some value d.
+ * 
+ * SYNC: (a, b, c) => d
+ * 
+ * There might be an asynchronous, callback version of this that would take those same parameters but also a fourth parameter: a node-style callback.
+ * 
+ * ASYNC: (a, b, c, callback) => void
+ * 
+ * (Small aside: the word "void" means that the function doesn't return any value.)
+ * 
+ * The node-style callback has a simple signature: (error, value) => void.  If there is a value for the error parameter, that means that the async process failed and we should handle the error.  If there is no error, that means the async process was successful, and we should process the result.
+ * 
+ * In other words, this method might not return d, but it will call the callback with d as a parameter.
+ * 
+ * Promises are another take on this.  Our method signature is now:
+ * 
+ * ASYNC: (a, b, c) => Promise<d>
+ * 
+ * This challenge is to take the callback version of this function, all the necessary params, and return a promise.
+ * 
+ * @param {CallbackStyleAsyncFunction} fn
+ * @param {*} fnParams 
+ * @return {Promise<any, any>}
+ */
+function makePromiseFromFunctionWithCallback(fn, ...fnParams){
+  /* Call fn with the fn params and a Node-style callback. */
+}
+
+/**
+ * @callback CallbackStyleAsyncFunction
+ * @param {*} fnParams
+ * @param {NodeStyleCallback} cb
+ */
+/**
+ * @callback NodeStyleCallback
+ * @param {Error} error
+ * @param {any} result
+ * @returns {void}
+ */
+
 module.exports = {
   makePromiseResolveWith3,
   makePromiseRejectWithBoo,
   makePromiseWithConstructor,
+  makeDelayPromise,
+  makePromiseFromFunctionWithCallback,
 };
--- test/warmup.test.js
+++ test/warmup.test.js
@@ -3,34 +3,98 @@ const assert = require('assert');
 const {
   makePromiseResolveWith3,
   makePromiseRejectWithBoo,
-  makePromiseWithConstructor
+  makePromiseWithConstructor,
+  makeDelayPromise,
+  makePromiseFromFunctionWithCallback,
 } = require('../answers/warmup');
+describe('Promise.resolve: (val:T) => Promise<T>', () => {
+  describe('#makePromiseResolveWith3:() => Promise<number>', () => {
+    it('creates a resolving promise', () => {
+      return makePromiseResolveWith3()
+        .then((val) => {
+          assert.equal(val, 3);
+        });
+    });
+  });
+});
 
-describe('Promise.resolve(val)', () => {
-  it('creates a resolving promise', () => {
-    return makePromiseResolveWith3()
-      .then((val) => {
-        assert.equal(val, 3);
-      });
+describe('Promise.reject: (err:T) => Promise<,T>', () => {
+  describe('#makePromiseRejectWithBoo:() => Promise<,string>', () => {
+    it('creates a rejecting promise', () => {
+      return makePromiseRejectWithBoo()
+        .then(() => {
+          assert.fail('This promise should have rejected, not resolved');
+        }, (err) => {
+          assert.equal(err, 'Boo!');
+        });
+    });
   });
 });
-describe('Promise.reject(err)', () => {
-  it('creates a rejecting promise', () => {
-    return makePromiseRejectWithBoo()
-      .then(() => {
-        assert.fail('This promise should have rejected, not resolved');
-      }, (err) => {
-        assert.equal(err, 'Boo!');
-      });
+describe('Promise constructor "new Promise((resolve, reject) => void) => Promise', () => {
+  describe('#makePromiseWithConstructor: (boolean) => Promise<undefined,undefined>', () => {
+    it('creates a promise that will resolve', () => {
+      return makePromiseWithConstructor(true);
+    });
+    it('creates a promise that will reject', () => {
+      return makePromiseWithConstructor(false)
+        .then(() => {
+          assert.fail('This should have failed.');
+        }, () => {});
+    });
   });
-});
-describe('Promise constructor "new Promise((resolve, reject) => void)', () => {
-  it('creates a promise that can resolve or reject', () => {
-    const shouldResolve = makePromiseWithConstructor(true);
-    const shouldReject = makePromiseWithConstructor(false)
-      .then(() => {assert.fail('This should have rejected');},() => {});
-    return Promise.all([shouldResolve, shouldReject]);
+  describe('#makeDelayPromise: (A, number) => Promise<A>', () =>{
+    it('returns a promise that resolves with the value', () => {
+      const start = new Date();
+      return makeDelayPromise('Hello', 0)
+        .then((val) => {
+          assert.equal(val, 'Hello');
+          assert((new Date() - start) < 50, 'Took too long');
+        });
+    });
+    it('returns a promise that resolves after a delay', () => {
+      const start = new Date();
+      return makeDelayPromise('World', 1000)
+        .then((val) => {
+          assert.equal(val, 'World');
+          const diff = new Date() - start;
+          assert(diff >= 1000, 'Happened too early');
+          assert(diff < 1050, 'Took too long');
+        });
+    });
   });
-  it('can be used to turn a callback method into a promise');
-  it('can be combined with setTimeout to create a delayed promise');
+  describe('#makePromiseFromFunctionWithCallback:(fn:([...params,] cb: (err:E, val:T) => void)) => void)=>([...params]) => Promise<T,E>', () => {
+    it('turns a resolving callback function into a resolving promise', () => {
+      function resolver(cb){
+        cb(null, 'Hello');
+      }
+      return makePromiseFromFunctionWithCallback(resolver)
+        .then(val => {
+          assert.equal(val, 'Hello');
+        });
+    });
+    it('turns a rejecting callback function into a rejecting promise', () => {
+      function rejector(cb){
+        cb('Boo!');
+      }
+      return makePromiseFromFunctionWithCallback(rejector)
+        .then(() => {
+          assert.fail('This should have rejected');
+        }, (err) => {
+          assert.equal(err, 'Boo!');
+        });
+    });
+
+    it('takes params in the returning function', () => {
+      function delayedResolver(firstVal, secondVal, cb){
+        setTimeout(() => {
+          cb(null, firstVal + ' ' + secondVal);
+        }, 500);
+      }
+
+      return makePromiseFromFunctionWithCallback(delayedResolver, 'Hello', 'World')
+        .then((val) => {
+          assert.equal(val, 'Hello World');
+        });
+    });
+  });
 });
