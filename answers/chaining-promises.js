037a81c294c1d7ef610e4c34a9436847ef268eb2
JoelCodes <amnshinness@yahoo.com>
2018-09-19 13:46:27 -0700

    Adds text to explain chaining two async processes.

--- answers/chaining-promises.js
+++ answers/chaining-promises.js
@@ -9,7 +9,7 @@
  * This chaining is a common problem in async, so let's create a function to do this.
  * 
  * @param {Promise} promise 
- * @param {*} asyncTransformer 
+ * @param {function} asyncTransformer 
  */
 function flatMapPromise(promise, asyncTransformer){
   return new Promise((resolve, reject) => {
@@ -18,7 +18,24 @@ function flatMapPromise(promise, asyncTransformer){
   });
 }
 
+/**
+ * 
+ * EXERCISE 2
+ * 
+ * I'm sure you're already a few steps ahead of me here, but just like with transforming promises with synchronous promises, we have a method to do this: .then(cb)!  
+ * 
+ * If you return a promise from your callback to .then, you won't have a promise of a promise of some value.  It'll just kinda smoosh them together, so a promise of a promise of a promise of an array just becomes a promise of an array.
+ * 
+ * Let's practice that!  You're going to create an example like the one above, which will use .then(cb) to transform the value of a promise using another async process.  It's a little hairy, but we'll start out with some code.
+ * 
+ * @param {Promise} firstPromise 
+ * @param {function} slowAsyncProcess 
+ */
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
+  return firstPromise.then(/* IMPLEMENT ME! */);
+}
 
 module.exports = {
   flatMapPromise,
+  chainTwoAsyncProcesses,
 };
--- test/chaining-promises.test.js
+++ test/chaining-promises.test.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 
 const assert = require('assert');
-const {flatMapPromise} = require('../answers/chaining-promises.js');
+const {flatMapPromise, chainTwoAsyncProcesses} = require('../answers/chaining-promises.js');
 
 describe('Chaining Promises with .then(cb) and .catch(cb)', () => {
   describe('#flatMapPromise(promise, asyncTransformer) => Promise', () => {
@@ -33,6 +33,33 @@ describe('Chaining Promises with .then(cb) and .catch(cb)', () => {
             assert.equal(err, 'Boo!');
           });
       });
+    });
+  });
+  
+  describe('#chainTwoAsyncProcesses(firstPromise, slowAsyncProcess)', () => {
+    it('runs a slow process on the result of the numberPromise', () => {
+      const time = new Date();
+      const numberPromise = Promise.resolve(31);
+
+      function slowSquarer(num){
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(num * num);
+          }, 1000);
+        });
+      }
+
+      return chainTwoAsyncProcesses(numberPromise, slowSquarer)
+        .then((val) => {
+
+          assert.equal(val, 961);
+
+          const timeElapsed = new Date() - time;
+
+          assert(timeElapsed >= 975, 'Process too quick.  Are you sure that you chained the two processes?');
+
+          assert(timeElapsed <= 1025, 'Process too slow.');
+        });
     });
   });
 });
