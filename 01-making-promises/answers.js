e6c28f5ffea0c5c567aa38c551e911ea7f4db8b7
Michael Byalsky <64349105+michaelbyalsky@users.noreply.github.com>
2020-08-05 15:16:25 +0300

    done exercise 1

--- 01-making-promises/answers.js
+++ 01-making-promises/answers.js
@@ -1,49 +1,59 @@
-
 /**
- * 
+ *
  * EXERCISE 1
- * 
+ *
  * @returns {Promise<3>}
  */
-function makePromiseResolveWith3(){
+function makePromiseResolveWith3() {
   /* IMPLEMENT ME! */
-}
-
-/**
- * 
- * EXERCISE 2
- * 
- * @returns {Promise<,"Boo!">}
- */
-function makePromiseRejectWithBoo(){
-  /* IMPLEMENT ME! */
-}
-
-/**
- * 
- * EXERCISE 3
- * 
- * @param {boolean} itShouldResolve - Whether the promise should resolve or reject
- * @returns {Promise<undefined, undefined>}
- */
-
-function makePromiseWithConstructor(itShouldResolve){
   return new Promise((resolve, reject) => {
-    /* If itShouldResolve is true, call resolve */
-    /* If itShouldResolve is false, call reject */
+    resolve(3);
   });
 }
 
 /**
- * 
+ *
+ * EXERCISE 2
+ *
+ * @returns {Promise<,"Boo!">}
+ */
+function makePromiseRejectWithBoo() {
+  /* IMPLEMENT ME! */
+  return new Promise((resolve, reject) => {
+    reject("Boo!");
+  });
+}
+/**
+ *
+ * EXERCISE 3
+ *
+ * @param {boolean} itShouldResolve - Whether the promise should resolve or reject
+ * @returns {Promise<undefined, undefined>}
+ */
+
+function makePromiseWithConstructor(itShouldResolve) {
+  return new Promise((resolve, reject) => {
+    /* If itShouldResolve is true, call resolve */
+    /* If itShouldResolve is false, call reject */
+    itShouldResolve ? resolve(3) : reject("Boo!");
+  });
+}
+
+/**
+ *
  * EXERCISE 4
  *
- * @param {any} value 
- * @param {number} delayInMs 
+ * @param {any} value
+ * @param {number} delayInMs
  * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
  */
-function makeDelayPromise(value, delayInMs){
+function makeDelayPromise(value, delayInMs) {
   /* Return a promise that resolves with the value after delayInMs */
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delayInMs);
+  });
 }
 
 module.exports = {
