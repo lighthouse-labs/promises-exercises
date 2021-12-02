f9d43f9ed7d691dc197ff806fad7410cf41488fe
lara <azizasayedahmad@gmail.com>
2021-12-02 13:28:01 +0200

    consuming-promises

--- 02-consuming-promises/answers.js
+++ 02-consuming-promises/answers.js
@@ -1,24 +1,26 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
+ *
  * @param {Promise} promise
  * @param {thunk} action
- * 
+ *
  */
-function waitForPromise(promise, action){
+function waitForPromise(promise, action) {
   /* IMPLEMENT ME */
+  return promise.then(action);
 }
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} promise 
- * @param {consumer} consumer 
- * @param {handler} handler 
+ *
+ * @param {Promise} promise
+ * @param {consumer} consumer
+ * @param {handler} handler
  */
-function consumePromise(promise, consumer, handler){
+function consumePromise(promise, consumer, handler) {
   /* IMPLEMENT ME! */
+  promise.then(consumer).catch(handler);
 }
 
 /**
