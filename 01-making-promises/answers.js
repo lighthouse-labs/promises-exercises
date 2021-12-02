ebc7aef5a8609a5ed9e993352d3bf9621e9c9471
lara <azizasayedahmad@gmail.com>
2021-12-02 13:12:12 +0200

    making exercises

--- 01-making-promises/answers.js
+++ 01-making-promises/answers.js
@@ -1,49 +1,60 @@
-
 /**
- * 
+ *
  * EXERCISE 1
- * 
+ *
  * @returns {Promise<3>}
  */
-function makePromiseResolveWith3(){
+function makePromiseResolveWith3() {
   /* IMPLEMENT ME! */
+  return Promise.resolve(3);
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
+ *
  * @returns {Promise<,"Boo!">}
  */
-function makePromiseRejectWithBoo(){
+function makePromiseRejectWithBoo() {
   /* IMPLEMENT ME! */
+  return Promise.reject("Boo!");
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
+ *
  * @param {boolean} itShouldResolve - Whether the promise should resolve or reject
  * @returns {Promise<undefined, undefined>}
  */
 
-function makePromiseWithConstructor(itShouldResolve){
+function makePromiseWithConstructor(itShouldResolve) {
   return new Promise((resolve, reject) => {
     /* If itShouldResolve is true, call resolve */
     /* If itShouldResolve is false, call reject */
+    if (itShouldResolve) {
+      resolve("truthy");
+    } else {
+      reject("falsy");
+    }
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 4
  *
- * @param {any} value 
- * @param {number} delayInMs 
+ * @param {any} value
+ * @param {number} delayInMs
  * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
  */
-function makeDelayPromise(value, delayInMs){
+function makeDelayPromise(value, delayInMs) {
   /* Return a promise that resolves with the value after delayInMs */
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delayInMs);
+  });
 }
 
 module.exports = {
